d95ab62ded8ff5413fafd7fc53c3157843839dc8
kdmfuture <kdmfuture@gmail.com>
2021-04-25 14:16:08 +0300

    new tests added

--- test_product_page.py
+++ test_product_page.py
@@ -15,7 +15,7 @@ from .pages.product_page import ProductPage
                                                "/?promo=offer7", marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def est_guest_can_add_product_to_basket(browser, link):
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
