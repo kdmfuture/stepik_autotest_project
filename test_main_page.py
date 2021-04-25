05acf5b08cf846a9f522882934b8d02e1a2e1e06
kdmfuture <kdmfuture@gmail.com>
2021-04-25 20:44:57 +0300

    refactored with stepik reccomendations

--- test_main_page.py
+++ test_main_page.py
@@ -6,14 +6,14 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
+    page.go_to_registration_page()
 
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.should_be_registration_link()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
