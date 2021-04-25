17565024ddea2624bfb5d15e57109c0b7536787b
kdmfuture <kdmfuture@gmail.com>
2021-04-25 19:24:27 +0300

    new test added

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from .pages.basket_page import BasketPage
 from .pages.main_page import MainPage
 
 
@@ -13,3 +14,13 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page = BasketPage(browser, browser.current_url)
+    page.is_basket_empty()
+    page.empty_basket_text()
