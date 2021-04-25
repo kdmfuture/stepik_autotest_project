0cb0bab6a92b93f21b43653605ccd02b7388578d
kdmfuture <kdmfuture@gmail.com>
2021-04-25 14:33:38 +0300

    new tests added

--- test_product_page.py
+++ test_product_page.py
@@ -47,4 +47,18 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     assert page.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE)
 
 
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+
 
