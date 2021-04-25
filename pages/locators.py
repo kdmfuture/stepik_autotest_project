a76d2078ebddabe5e3bd36f934d7cf04e4134dc7
kdmfuture <kdmfuture@gmail.com>
2021-04-25 14:33:10 +0300

    basepage redesigned

--- pages/locators.py
+++ pages/locators.py
@@ -1,8 +1,8 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link ")
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link ")
 
 
 class LoginPageLocators:
